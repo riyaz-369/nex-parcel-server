ea6bf8fc14825a90fc9ef692c8bc664fbbe10b7c
Your Nameriyazulislam99 <riyazulislam0099@gmail.com>
2024-06-06 19:09:13 +0600

    gte and lte implement on bookings data

--- index.js
+++ index.js
@@ -6,7 +6,16 @@ const port = process.env.PORT || 5000;
 const app = express();
 
 // middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "http://localhost:5174",
+      "https://nex-parcel.web.app",
+      "https://nex-parcel.firebaseapp.com",
+    ],
+  })
+);
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.308otot.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -21,8 +30,10 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    const usersCollection = client.db("nexParcel").collection("users");
-    const bookingsCollection = client.db("nexParcel").collection("bookings");
+    const database = client.db("nexParcel");
+
+    const usersCollection = database.collection("users");
+    const bookingsCollection = database.collection("bookings");
 
     // USER RELATED APIS
     app.post("/users", async (req, res) => {
@@ -30,8 +41,7 @@ async function run() {
       const filter = { email: users?.email };
       const exist = await usersCollection.findOne(filter);
       if (exist) return res.send({ message: "Already exist" });
-      const options = { upsert: true };
-      const result = await usersCollection.insertOne(users, options);
+      const result = await usersCollection.insertOne(users);
       res.send(result);
     });
 
@@ -55,30 +65,67 @@ async function run() {
       res.send(result);
     });
 
-    // MAKE DELIVERYMEN A USER
-    app.patch("/users/:id", async (req, res) => {
-      const id = req.params.id;
-      const userRole = req.body;
-      const filter = { _id: new ObjectId(id) };
+    // MAKE A USER DELIVERYMEN OR ADMIN
+    app.patch("/users/:email", async (req, res) => {
+      const email = req.params.email;
+      console.log(email);
+      const userInfo = req.body;
+      const filter = { email: email };
+      const options = { upsert: true };
       const updateDoc = {
         $set: {
-          role: userRole.role,
+          ...userInfo,
         },
       };
-      const result = await usersCollection.updateOne(filter, updateDoc);
+      const result = await usersCollection.updateOne(
+        filter,
+        updateDoc,
+        options
+      );
+      res.send(result);
+    });
+
+    // UPDATE ONE NO OF DELIVERED PARCEL
+    app.put("/user/:email", async (req, res) => {
+      const email = req.params.email;
+      const filter = { email: email };
+      const options = { upsert: true };
+      const updateDoc = {
+        $inc: { no_of_delivered_parcel: 1 },
+      };
+      const result = await usersCollection.updateOne(
+        filter,
+        updateDoc,
+        options
+      );
       res.send(result);
     });
 
     // PARCEL BOOKING RELATED APIS
     app.get("/bookings/:email", async (req, res) => {
       const email = req.params.email;
-      const result = await bookingsCollection.find({ email }).toArray();
+      const filter = req.query.filter;
+      let query = { email: email };
+      if (filter) {
+        query.status = filter;
+      }
+      const result = await bookingsCollection.find(query).toArray();
       res.send(result);
     });
 
     // GET ALL BOOKING PARCEL FOR ADMIN
     app.get("/bookings", async (req, res) => {
-      const bookings = await bookingsCollection.find().toArray();
+      const fromDate = req.query.fromDate;
+      const toDate = req.query.toDate;
+
+      let query = {};
+      if (fromDate && toDate) {
+        query.requested_delivery_date = {
+          $gte: fromDate,
+          $lte: toDate,
+        };
+      }
+      const bookings = await bookingsCollection.find(query).toArray();
       res.send(bookings);
     });
 
@@ -103,7 +150,6 @@ async function run() {
     // POST A BOOKING
     app.post("/bookings", async (req, res) => {
       const bookingData = req.body;
-      console.log(bookingData);
       const result = await bookingsCollection.insertOne(bookingData);
       res.send(result);
     });
@@ -115,21 +161,35 @@ async function run() {
       res.send(result);
     });
 
+    // // GET BOOKING BY STATUS
+    // app.get("/filter-by-status", async (req, res) => {
+    //   const filter = req.query.filter;
+    //   let query = {};
+    //   if (filter) {
+    //     query = { status: filter };
+    //   }
+    //   console.log(query);
+    //   const result = await bookingsCollection.find(query).toArray();
+    //   console.log(result);
+    //   res.send(result);
+    // });
+
     // UPDATE A BOOKING
     app.put("/bookings/:id", async (req, res) => {
       const updateData = req.body;
       const id = req.params.id;
-      console.log(id);
-      console.log(updateData);
       const filter = { _id: new ObjectId(id) };
-      console.log(filter);
+      const options = { upsert: true };
       const updateDoc = {
         $set: {
           ...updateData,
         },
       };
-      const result = await bookingsCollection.updateOne(filter, updateDoc);
-      console.log(result);
+      const result = await bookingsCollection.updateOne(
+        filter,
+        updateDoc,
+        options
+      );
       res.send(result);
     });
 
